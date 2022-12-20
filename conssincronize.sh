a3d053ad8d9af034f5597a7d6705aa08b3ad219f
Bashy <petitpierre@duck.com>
2022-12-20 08:39:37 +0100

    Add files via upload

--- conssincronize.sh
+++ conssincronize.sh
@@ -3,11 +3,11 @@
 #  Filename ~  conssincronize.sh           [ Created: 2022-12-19 | 17:20 ]  #
 #---[Author of this file]---------------------------------------------------#
 #  Jonas Petitpierre ~  @jonas52 -> https://github.com/jonas52
-NAS_USERNAME="nom dutilisateur du NAS"
-NAS_PASSWORD="mot de passe du NAS"
-NAS_IP="adresse IP du NAS"
-NAS_DEST_DIR="Path du repertoire"
+NAS_USERNAME="username_nas"
+NAS_PASSWORD="password_nas"
+NAS_IP="ip_nas"
+NAS_DEST_DIR="path to be sincronized"
 
 sshpass -p $NAS_PASSWORD ssh $NAS_USERNAME@$NAS_IP
 
-rsync -avz /Path/vers/les/fichiers/a/copier/ $NAS_USERNAME@$NAS_IP:$NAS_DEST_DIR
+rsync -avz /Path/to/the/files/tobe/copied/ $NAS_USERNAME@$NAS_IP:$NAS_DEST_DIR
